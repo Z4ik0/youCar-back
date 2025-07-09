22b0299959954e6bfa4b4c7e5e70fa9bf0f6adab
Irving Cruz Chávez <ic3386941@gmail.com>
2025-07-09 14:38:38 -0600

    [ADD]: Se agregaron archivos de para poder probrar los endpoints (formulario.html con integracion de calendly y email.html) además, se agrego el endpoint para el metodo post que utiliza nodemailer para enviar correos al recibir los datos del formulario

--- server.js
+++ server.js
@@ -13,6 +13,53 @@ app.get('/', function(req, res){
     })
 })
 
+app.get('/', function(req, res){
+    res.status(200).send({
+        message: "Servidor funcionando"
+    })
+})
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'ic3386941@gmail.com',
+        pass: 'xnou rdhe pkzq iddf'
+    }
+});
+
+
+app.post('/enviarformulario', async (req, res) => {
+    const { nombre, email, mensaje } = req.body;
+
+    // Guardar datos en js
+    let datos = [];
+    if (fs.existsSync('./datos.js')) {
+        delete require.cache[require.resolve('./datos.js')];
+        datos = require('./datos.js');
+    }
+    datos.push({ nombre, email, mensaje });
+    fs.writeFileSync('./datos.js', 'module.exports = ' + JSON.stringify(datos, null, 2) + ';');
+
+    const mailOptions = {
+        from: 'ic3386941@gmail.com',
+        to: 'josedejesuszual2004@correo.com',
+        subject: 'Nuevo formulario recibido',
+        text: `Nombre: ${nombre}\nEmail: ${email}\nMensaje: ${mensaje}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ mensaje: 'Correo enviado correctamente' });
+        console.log('Correo enviado exitosamente')
+    } catch (error) {
+        res.status(500).json({ error: 'Error al enviar el correo', detalle: error.toString() });
+    }
+});
+
+
 app.listen(port, () => {
     console.log(`Server running in http://localhost:${port}`);
 });
+
